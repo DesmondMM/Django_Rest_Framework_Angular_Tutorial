2a7115955cee81b4287ef8ab20e7e3bee5d31178
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-08 08:34:59 +0200

    Created Questions Template

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,20 +4,10 @@
 
 var pollcatApp = angular.module('pollcatApp', []);
 
-pollcatApp.controller('PollListCtrl', function($scope) {
-  $scope.questions = [
-    {'id': 1,
-     "question_text": "zHello what's up?",
-    "pub_date": "2015-12-28T21:45:41Z"},
+pollcatApp.controller('PollListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://127.0.0.1:8080/?format=json').success(function(data) {
+    $scope.questions = data;
+  });
 
-    {"id": 2,
-        "question_text": "How can i help you",
-        "pub_date": "2015-12-30T04:34:57Z"},
-
-    {"id": 3,
-        "question_text": "Who is your favorite Beatle?",
-        "pub_date": "2015-12-29T04:57:20.466289Z"}
-
-  ];
   $scope.orderProp = 'id';
-});
+}]);
