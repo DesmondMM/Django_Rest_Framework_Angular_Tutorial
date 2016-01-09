bcf4930b1df7c06c27e96a0189c9b36eee983bcd
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-09 15:12:57 +0200

    Added View Questions Template

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,58 @@
 'use strict';
 
-/* App Module */
+var pollcatApp = angular.module('pollcatApp', [
+  'ngRoute',
+
+]);
+
+pollcatApp.controller('PollListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://127.0.0.1:8000/?format=json').success(function(data) {
+    $scope.questions = data;
+  });
+
+  $scope.orderProp = 'id';
+}]);
+
+pollcatApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://localhost:8000').success(function(data) {
+    $scope.questions = data;
+  });
+
+  $scope.orderProp = 'id';
+}]);
+
+pollcatApp.controller('PollListController', function($scope){
+
+})
+
+.config(['$routeProvider',function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        template: '<h1>Welcome to Polls App</h1>',
+        controller: 'PollListController'
+      }).
+      when('/questions', {
+        templateUrl: 'partials/question-list.html',
+        controller: 'QuestionListCtrl'
+      }).
+      when('/questions/:id', {
+        templateUrl: 'partials/question-list.html',
+        controller: 'QuestionListCtrl'
+      }).
+      when('/add', {
+        templateUrl: 'partials/add-question.html',
+        controller: 'PollListController'
+      }).
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'PollListController'
+      }).
+      when('/error', {
+        template: '<h1>404: Sorry an Error Occured</h1>',
+        controller: 'PollListController'
+      }).
+      otherwise({
+        redirectTo: '/error'
+      });
+
+  }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,4 @@
 
 /* Controllers */
 
-var pollcatApp = angular.module('pollcatApp', []);
 
-pollcatApp.controller('PollListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://127.0.0.1:8080/?format=json').success(function(data) {
-    $scope.questions = data;
-  });
-
-  $scope.orderProp = 'id';
-}]);
