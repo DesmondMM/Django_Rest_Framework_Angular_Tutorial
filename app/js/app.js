9157c497b8ce9bf21834942adf58f4348fe213ac
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-10 06:23:01 +0200

    Removed controllers from app.js and added them to controllers.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,30 +1,13 @@
 'use strict';
 
-var pollcatApp = angular.module('pollcatApp', [
+var pollecatApp = angular.module('phonecatApp', [
   'ngRoute',
+  'pollcatAnimations',
 
-]);
-
-pollcatApp.controller('QuestionDetailCtrl', function($scope, $http, $routeParams){
-    $http.get('http://localhost:8000/' + $routeParams.id).success(function(data) {
-    $scope.detail = data;
-    });
-
-  });
-
-
-pollcatApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://localhost:8000').success(function(data) {
-    $scope.questions = data;
-
-  });
-
-  $scope.orderProp = 'id';
-}]);
-
-pollcatApp.controller('PollListController', function($scope){
-
-})
+  'pollcatControllers',
+  'pollcatFilters',
+  'pollcatServices'
+])
 
 .config(['$routeProvider',function($routeProvider) {
     $routeProvider.
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,30 @@
 'use strict';
 
-/* Controllers */
+var pollcatApp = angular.module('pollcatApp', [
+  'ngRoute',
+
+]);
+
+pollcatApp.controller('QuestionDetailCtrl', function($scope, $http, $routeParams){
+    $http.get('http://localhost:8000/' + $routeParams.id).success(function(data) {
+    $scope.detail = data;
+    });
+
+  });
+
+
+pollcatApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://localhost:8000').success(function(data) {
+    $scope.questions = data;
+
+  });
+
+  $scope.orderProp = 'id';
+}]);
+
+pollcatApp.controller('PollListController', function($scope){
+
+})
+
 
 
